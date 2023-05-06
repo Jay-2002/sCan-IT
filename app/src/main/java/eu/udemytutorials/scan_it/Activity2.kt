66b7c0a9a1b566d854e76b2c819e8c238c3ecbbe
Jay-2002 <jnsawant_b20@ce.vjti.ac.in>
2023-05-06 20:18:21 +0530

    update activity 2
    no image found

--- app/src/main/java/eu/udemytutorials/scan_it/Activity2.kt
+++ app/src/main/java/eu/udemytutorials/scan_it/Activity2.kt
@@ -5,23 +5,40 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Toast
 
 class Activity2 : AppCompatActivity() {
 
+    private lateinit var imageView : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
 
-        val imageView = findViewById<ImageView>(R.id.imgview1)
-        val imageBitmap = intent.getParcelableExtra<Bitmap>("imageBitmap")
+        imageView = findViewById(R.id.imgview1)
+
+        // Retrieve the image URI or bitmap from the intent
         val imageUriString = intent.getStringExtra("imageUri")
-        if (imageBitmap != null) {
+        val imageBitmap = intent.getParcelableExtra<Bitmap>("imageBitmap")
+
+        // Set the image to the imageView
+        if (imageUriString != null) {
+            imageView.setImageURI(Uri.parse(imageUriString))
+        } else if (imageBitmap != null) {
             imageView.setImageBitmap(imageBitmap)
-        } else if (imageUriString != null) {
-            val imageUri = Uri.parse(imageUriString)
-            imageView.setImageURI(imageUri)
+        } else {
+            Toast.makeText(this, "No image selected", Toast.LENGTH_SHORT).show()
         }
-    
+
+//        val imageView = findViewById<ImageView>(R.id.imgview1)
+//        val imageBitmap = intent.getParcelableExtra<Bitmap>("imageBitmap")
+//        val imageUriString = intent.getStringExtra("imageUri")
+//        if (imageBitmap != null) {
+//            imageView.setImageBitmap(imageBitmap)
+//        } else if (imageUriString != null) {
+//            val imageUri = Uri.parse(imageUriString)
+//            imageView.setImageURI(imageUri)
+//        }
+
     }
 }
